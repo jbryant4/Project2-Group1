f59a6d25769b74acba54d6145f75d0cfe11648b7
JoKing <joebryant614@gmail.com>
2021-06-16 23:33:19 -0700

    model associations

--- models/index.js
+++ models/index.js
@@ -1,21 +1,97 @@
+//! import all the models
+const User = require('./User');
+const List = require('./List');
+const Movies = require('./Movies');
+const Comments = require('./Comments');
+const Vote = require('./Vote');
 
+//!Associations
 //user has many list 
+User.hasMany(List, {
+    foreignKey: 'user_id'
+});
+
 // list belongs to user
-// user has many users 
+List.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+//! need help user to user association
+// user has many users
+User.hasMany(User);
+User.belongsToMany(User);
 
 // user belongs to many lists through vote (ref)
+User.belongsToMany(List, {
+    through: Vote,
+    as: 'voted_lists',
+
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
 // list belongs to many users through vote (ref)
+List.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_lists',
+
+    foreignKey: 'list_id',
+    onDelete: 'SET NULL'
+});
 
 //list has many movies
+List.hasMany(Movies, {
+    foreignKey: 'list_id'
+});
+
 // movies belongs to many lists 
+Movies.belongsToMany(List, {
+    foreignKey: 'list_id'
+});
 
 // vote belongs to user
-// vote belongs to list 
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
-// user has many votes 
-//post has many votes
+// vote belongs to list 
+Vote.belongsTo(List, {
+    foreignKey: 'list_id'
+});
+
+// user has many votes
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+//list has many votes
+List.hasMany(Vote, {
+    foreignKey: 'list_id'
+});
 
 // comments belongs to user
+Comments.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
 // comment belongs to list
+Comments.belongsTo(List, {
+    foreignKey: 'list_id',
+    onDelete: 'SET NULL'
+});
+
 // user has many comments 
+User.hasMany(Comments, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+});
+
 // list has many comments
+List.hasMany(Comments, {
+    foreignKey: 'list_id',
+    onDelete: 'SET NULL'
+});
+
+//! export all models
+module.exports = { User, List, Movies, Comments, Vote }
