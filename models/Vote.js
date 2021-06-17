9c2f3ec922b0c226b91a11ba398ee37dbf916efb
JoKing <joebryant614@gmail.com>
2021-06-16 23:35:55 -0700

    Vote Model

--- models/Vote.js
+++ models/Vote.js
@@ -1,4 +1,41 @@
+//add sequelize and the connection
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// attach Vote to sequelize Mmodel 
+class Vote extends Model { }
+
 //Vote Model
-    
-    // id, user_id, list_id 
-    
+// id, user_id, list_id 
+Vote.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        list_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'list',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'vote'
+    }
+);
+
+module.exports = Vote;
