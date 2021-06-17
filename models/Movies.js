0c098189c46ae3c0873aaf1d0f4d089c108bd7fc
JoKing <joebryant614@gmail.com>
2021-06-16 23:52:18 -0700

    movie and list model

--- models/Movies.js
+++ models/Movies.js
@@ -1,3 +1,50 @@
-// Movies Model 
 
-    // id, movieTitle, year, genre (whatever else we want to pull from the api), list_id
+//add sequelize and the connection
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// attach Vote to sequelize Mmodel 
+class Movies extends Model { }// Movies Model 
+
+// id, movieTitle, year, genre (whatever else we want to pull from the api), list_id
+Movies.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        movieTitle: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        genre: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        year: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'list'
+    }
+);
+
+module.exports = List;
