2041c336160d622bc1168148fa744d0df665eb1b
Kellie Werrell <kelliewerrell@gmail.com>
2021-06-17 20:29:42 -0700

    added changes to thirdpartyapi call * tested in insomnia

--- utils/thirdPartyApi.js
+++ utils/thirdPartyApi.js
@@ -1,15 +1,13 @@
 const router = require('express').Router();
-const bodyParser = require('body-parser');
-const request = require('request');
 const sequelize = require('../../config/connection');
 const db = require('../models');
-const apiKey = process.env.APIKEY;
+const apiKey = process.env.DB_APIKEY;
 
 router.get('/', function (req, res) { res.render('index'); });
 
 router.get('/search', function (req, res) {
     const query = req.query.q;
-    request('http://www.omdbapi.com/?s=' + query + "&apikey=" + apiKey, function (err, response, body)
+    fetch('http://www.omdbapi.com/?s=' + query + "&apikey=" + apiKey, function (err, response, body)
      { 
          let json = JSON.parse(body); 
          if (!err && json.Search)
