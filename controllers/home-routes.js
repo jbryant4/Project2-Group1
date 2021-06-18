8228f6bfbfdf3e1616281c33cf0b0d1f71327046
pablo de la cruz <pablodlc@gmail.com>
2021-06-17 20:57:12 -0700

    added hbs file structure, public folder, and start of public JS from Just-Tech-News

---
+++ controllers/home-routes.js
@@ -0,0 +1,103 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+
+// PD: I just included all of the Models here to be safe.  I'm thinking the others that are gray are rightly so and can be deleted here. But I'm not sure and I wanted more eyes here.
+const { Follower, List, ListContent, Movie, User, Comment, Vote } = require('../models');
+
+// get all posts for homepage
+router.get('/', (req, res) => {
+    console.log('======================');
+    List.findAll({
+        attributes: [
+            'id',
+            'title',
+            'movie_list',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE list.id = vote.list_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'list_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbListData => {
+            const lists = dbListData.map(list => list.get({ plain: true }));
+
+            res.render('homepage', {
+                lists,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// get single post
+router.get('/list/:id', (req, res) => {
+    List.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'list_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE list.id = vote.list_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'list_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbListData => {
+            if (!dbListData) {
+                res.status(404).json({ message: 'No list found with this id' });
+                return;
+            }
+
+            const post = dbListData.get({ plain: true });
+
+            res.render('single-list', {
+                list,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
+
+module.exports = router;
